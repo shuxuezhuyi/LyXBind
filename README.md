b36dc013b6aafb124a12d8b1a97a0198c50fc626
数学主义 <mathist@139.com>
2021-05-12 23:04:53 +0800

    用python3的pykeyboard在Linux中消除LyX响应单按Alt键的影响

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ LyXBind
 
 lyx.ahk 是 AutoHotkey 脚本. 请到 https://autohotkey.com/ 下载安装 AutoHotkey 以使用它.
 
+lyx.py 是 python3 脚本, 目前与 lyx.ahk 的功能重叠. 需要安装依赖包: `pip3 install PyUserInput`
+
 用 template.lyx 新建文档以充分利用这些快捷键. 此后生成 tex(XeTeX) 文档后, 可以用 macros.py 展开宏, 从而避免合并别人的文档时发生宏定义的冲突. 用法是: `python3 macros.py 你的tex文件`
---
+++ lyx.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from pykeyboard import *
+
+class ShortcutForLyX(PyKeyboardEvent):
+    def __init__(self):
+        PyKeyboardEvent.__init__(self)
+        self.SingleAlt = 0  # PreventSingleAlt 要用
+        self.kbd = PyKeyboard()
+        
+    def PreventSingleAlt(self, character, press):   # 本意是想阻止LyX响应单按Alt的事件, 但现在只是绕弯子消除影响而已
+        if (character == 'Alt_L' or character == 'Alt_R'):
+            if press == True:
+                if self.SingleAlt == 0: self.SingleAlt =+ 1
+                else: self.SingleAlt = 0
+            elif self.SingleAlt == 1:
+                self.kbd.tap_key(self.kbd.alt_key)  # 抢在下次键盘事件之前触发一次按下Alt键又松开的事件
+                self.SingleAlt =+ 1
+        else: self.SingleAlt = 0
+
+    def tap(self, keycode, character, press):
+        self.PreventSingleAlt(character, press)
+        print(keycode, character, press)
+        
+    def escape(self, event):    # 不用escape退出脚本
+        print('no escape')
+
+sample = ShortcutForLyX()
+sample.run()
